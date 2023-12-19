eb6b4f4644dc529d6a148bbc25a96b43d2891bf9
Lucas-Godoi <lucasdegodoimoraes@hotmail.com>
2023-12-19 12:04:16 +0000

    First commit :tada:

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float xmin, xmax, zmin, zmax;
+    public float speed;
+    public float tilt;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+
+    private float nextFire;
+
+    public float fireRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rb.velocity = movement * speed;
+
+        //Limitar o movimento da nave
+        rb.position = new Vector3(Mathf.Clamp(rb.position.x, xmin, xmax), 0, Mathf.Clamp(rb.position.z, 0, zmax));
+
+        // rodar a nave de acordo com a velocidade
+        rb.rotation = Quaternion.Euler(0,0,-(rb.velocity.x*tilt));
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
+}
---
+++ Assets/RandomRotator.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float tumble = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+
+
+}
