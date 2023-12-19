f55a47728cc15a7e3815c9fe071a12e3ab56679c
lucas <lucasoalzst@gmail.com>
2023-12-19 11:49:01 +0000

    shooting shots and boundary - Done!

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody rb;
+    public Rigidbody spaceship;
+    public AudioSource shootSound;
     public float speed;
     public float xmin,xmax, zmin,zmax;
     public float tilt;
 
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    private float nextFire;
+
     void Start(){
-        rb = GetComponent<Rigidbody>();
-        // shootSound = GetComponent<AudioSource>();
+        spaceship = GetComponent<Rigidbody>();
+        shootSound = GetComponent<AudioSource>();
+    }
+
+    void Update (){
+        if (Input.GetButton("Fire1") && Time.time> nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            //GetComponent<AudioSource>().Play;
+        }
     }
 
     void FixedUpdate()
@@ -20,11 +35,8 @@ public class PlayerController : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
         
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
-
-        rb.velocity = movement * speed;
-
-        rb.position = new Vector3(Mathf.Clamp(rb.position.x, xmin, xmax), 0, Mathf.Clamp(rb.position.z, zmin, zmax));
-
-        rb.rotation = Quaternion.Euler(0.0f, 0.0f, (rb.velocity.x * -tilt));
+        spaceship.velocity = movement * speed;
+        spaceship.position = new Vector3(Mathf.Clamp(spaceship.position.x, xmin, xmax), 0, Mathf.Clamp(spaceship.position.z, zmin, zmax));
+        spaceship.rotation = Quaternion.Euler(0.0f, 0.0f, (spaceship.velocity.x * -tilt));
     }
 }
