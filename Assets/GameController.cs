bf342e5cfa83a7bcfabd43800f1b01aa99eee2ae
lucasoal <lucasoalzst@gmail.com>
2024-01-09 12:09:50 +0000

    add insects

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject hazard;
+    public List<GameObject> hazards; // Lista de hazards
     public Vector3 spawnValues;
     public int hazardCount;
     public float spawnWait;
@@ -13,19 +13,23 @@ public class GameController : MonoBehaviour
 
     void Start()
     {
-        StartCoroutine (SpawnWaves());
+        StartCoroutine(SpawnWaves());
     }
 
     IEnumerator SpawnWaves()
     {
         yield return new WaitForSeconds(startWait);
-        
-        while(true){
+
+        while (true)
+        {
             for (int i = 0; i < hazardCount; i++)
             {
+                // Escolhe aleatoriamente um hazard da lista
+                GameObject hazardToSpawn = hazards[Random.Range(0, hazards.Count)];
+
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
-                Instantiate(hazard, spawnPosition, spawnRotation);
+                Instantiate(hazardToSpawn, spawnPosition, spawnRotation);
 
                 yield return new WaitForSeconds(spawnWait);
             }
